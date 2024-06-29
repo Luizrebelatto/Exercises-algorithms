bb59bac4b2d81164846756d1779a2f3134c1cf07
Luizrebelatto <lbrebelatto@gmail.com>
2024-06-29 10:07:33 -0300

    resolved 704 Binary Search

---
+++ 704-Binary-Search.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+    var left = 0
+    var right = nums.count - 1
+    
+    while left <= right {
+        var middle = (right + left) / 2
+        
+        if nums[middle] == target {
+            return middle
+        } else if nums[middle] < target {
+            left = middle + 1
+        } else {
+            right = middle - 1
+        }
+    }
+    return -1
+    }
+}
